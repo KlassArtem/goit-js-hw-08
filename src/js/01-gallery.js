b40bd66f9347e0ef7f518b62630ffa37c7b7b50c
KlassArtem <111056786+KlassArtem@users.noreply.github.com>
2023-01-10 11:06:01 +0200

    update 1rst

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallRef = document.querySelector('.gallery');
+
+function createGalleryItemMarkUp({ preview, original, description } = {}) {
+  return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+}
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItemMarkUp).join('');
+
+gallRef.innerHTML = galleryItemsMarkup;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
